6fe77e05842fb2e268493657438aacae552e7b1c
gary62107 <gary62107@gmail.com>
2014-06-11 16:27:13 +0800

    db changed

--- model/db.js
+++ model/db.js
@@ -1,11 +1,19 @@
 var mysql = require('mysql');
+// var connection = mysql.createConnection({
+// 	host: '140.124.181.190',
+// 	port: '3306',
+// 	user: 'keming',
+// 	password: 'root1421',
+// 	database: 'jumefood'
+// });
 var connection = mysql.createConnection({
-	host: '140.124.181.190',
-	port: '3306',
-	user: 'keming',
-	password: 'root1421',
+	host: '140.124.181.7',
+	port: '2819',
+	user: 'ISLab1221',
+	password: '1221',
 	database: 'jumefood'
 });
+
 connection.connect();
 
 exports.connection = connection;
